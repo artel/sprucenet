d86d2a3d27c78bf1db813c2ae747d8fc01558f78
artelus <me@artel.us>
2023-08-27 17:58:08 -0600

    strip

--- lib/seed.ts
+++ lib/seed.ts
@@ -13,32 +13,4 @@ export async function seed() {
     )
     .execute()
   console.log(`Created "users" table`)
-  const addUsers = await db
-    .insertInto('users')
-    .values([
-      {
-        name: 'Guillermo Rauch',
-        email: 'rauchg@vercel.com',
-        image:
-          'https://pbs.twimg.com/profile_images/1576257734810312704/ucxb4lHy_400x400.jpg',
-      },
-      {
-        name: 'Lee Robinson',
-        email: 'lee@vercel.com',
-        image:
-          'https://pbs.twimg.com/profile_images/1587647097670467584/adWRdqQ6_400x400.jpg',
-      },
-      {
-        name: 'Steven Tey',
-        email: 'stey@vercel.com',
-        image:
-          'https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg',
-      },
-    ])
-    .execute()
-  console.log('Seeded database with 3 users')
-  return {
-    createTable,
-    addUsers,
-  }
 }
